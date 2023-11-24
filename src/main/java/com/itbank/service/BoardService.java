ddd8766cfa27ce413f70b186ef39744c6b41a28d
lukby2457 <kjh7442455@gmail.com>
2023-11-24 13:04:47 +0900

    Logger 완료

--- src/main/java/com/itbank/service/BoardService.java
+++ src/main/java/com/itbank/service/BoardService.java
@@ -2,6 +2,9 @@ package com.itbank.service;
 
 import java.util.List;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +14,13 @@ import com.itbank.model.vo.BoardVO;
 @Service
 public class BoardService {
 	
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
 	@Autowired private BoardDAO dao;
 
 	public List<BoardVO> getBoards() {
+		log.debug("메서드 호출 됨~");
+		
 		return dao.selectAll();
 	}
 	
