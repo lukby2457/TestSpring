8f3e71a3f5b92d0268bcd62ed052b36e6d59623b
lukby2457 <kjh7442455@gmail.com>
2023-11-24 13:40:20 +0900

    AOP 완료

--- src/main/java/com/itbank/service/BoardService.java
+++ src/main/java/com/itbank/service/BoardService.java
@@ -2,9 +2,6 @@ package com.itbank.service;
 
 import java.util.List;
 
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,13 +11,9 @@ import com.itbank.model.vo.BoardVO;
 @Service
 public class BoardService {
 	
-	private Logger log = LoggerFactory.getLogger(getClass());
-	
 	@Autowired private BoardDAO dao;
 
 	public List<BoardVO> getBoards() {
-		log.debug("메서드 호출 됨~");
-		
 		return dao.selectAll();
 	}
 	
